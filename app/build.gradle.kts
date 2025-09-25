fc4d10c0c2057e272ef79f622226f27a9f61e59b
duyphuong0103 <nguyenthiduyphuong0103@gmail.com>
2025-09-25 23:05:22 +0700

    create LoginView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+
     }
 
     buildTypes {
@@ -32,7 +34,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
